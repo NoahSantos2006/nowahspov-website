c9395e7c2aca0660bbae4da0d7fd4275e762c139
NoahSantos2006 <noah13a@gmail.com>
2025-10-04 15:04:21 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
+const observer = new IntersectionObserver(entries => { // Intersection Overserver is a browser API that watched elements and tells when they enter and leave the viewport
+  entries.forEach(entry => {
+    if (entry.isIntersecting) { // When the element becomes visible
+      entry.target.classList.add('visible'); // adds 'visible' class to the img
+    }
+    else {
+        entry.target.classList.remove('visible');
+    }
+  });
+});
 
+document.querySelectorAll('.fade-on-scroll').forEach(img => { // selects all elements that have the class .fade-on-scroll
+  observer.observe(img);
+});
