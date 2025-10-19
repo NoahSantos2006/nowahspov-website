b2b6d8af8c70d467ae6517dbb8e3ba12b2552dd1
NoahSantos2006 <noah13a@gmail.com>
2025-10-18 22:31:03 -0700

    Add CSS styles and refactor image handling logic
    Refactor script.js to include CSS styles for layout and image effects.

--- public/script.js
+++ public/script.js
@@ -1,162 +1,184 @@
-const observer = new IntersectionObserver(entries => { // Intersection Overserver is a browser API that watched elements and tells when they enter and leave the viewport
-  entries.forEach(entry => {
-    if (entry.isIntersecting) { // When the element becomes visible
-      entry.target.classList.add('visible'); // adds 'visible' class to the img
-    }
-    else { //when the element is no longer visible
-        entry.target.classList.remove('visible');
-    }
-  });
-});
-
-
-document.querySelectorAll('.fade-on-scroll').forEach(img => { // selects all elements that have the class .fade-on-scroll
-  observer.observe(img);
-});
-
-// the function for determining the order of the lightbox images
-
-let arr_src = []
-let order = [];
-let hashmap = {};
-let col = [];
-let check = [];
-function galleryOrder() {
-  arr_src = []
-  var element = document.getElementsByClassName('grid-item');
-  for (const tag of element) {
-    const position = tag.getBoundingClientRect();
-    arr_src.push([tag.querySelector('img').src, position.x, position.y]);
-  }
-  order = [];
-  hashmap = {};
-  col = [];
-  check = [];
-  for (const [src, x, y] of arr_src) {
-    if (!(x in hashmap)) {
-      col.push(x);
-      hashmap[x] = [[src, y]];
-    } else {
-      hashmap[x].push([src, y]);
-    }
-  }
-  for (const val of col) {
-    check.push(hashmap[val][0]);
-  }
-  count = [0, 0, 0, 0]
-  while (1) {
-    let currMin = Infinity
-    allFalse = true
-    let temp = null
-    let src = null
-    let y = null
-    for (let i = 0; i < check.length; i++) {
-      if (!check[i]) {
-        continue
-      }
-      const[localSRC, localY] = check[i]
-
-      if (!localY) {
-        continue
-      }
-      if (localY < currMin) {
-        allFalse = false
-        temp = [i, check[i]]
-        src = localSRC
-        y = localY
-        currMin = y
-      }
-    }
-    if (allFalse) {
-      break
-    }
-    order.push(src)
-    count[temp[0]] += 1
-    if (count[temp[0]] >= hashmap[col[temp[0]]].length) {
-      check[temp[0]] = false
-    } else {
-      check[temp[0]] = hashmap[col[temp[0]]][count[temp[0]]]
-    }
-  }
+body {
+    background-color: #d0d9cd;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    overflow-x: hidden;
 }
 
-// whenever the document loads or resizes, a new gallery order is created in case x, y changes in the broswer
-['load', 'resize'].forEach(event => {
-  window.addEventListener(event, galleryOrder);
-});
+img {
+    max-width: 100%;
+    vertical-align: middle;
+    height: auto;
+}
 
-//makes the index-header fade out when the bottom section is reached
-window.addEventListener('scroll', () => { 
-  const curr_scroll = window.scrollY;
-  const vHeight = window.innerHeight;
-  const docHeight = document.documentElement.scrollHeight;
-  const header = document.querySelector(".index-header");
-  if (curr_scroll == docHeight - vHeight) {
-    header.classList.add('fade-out');
-    header.classList.remove('fade-in');
-  } else {
-    header.classList.remove('fade-out');
-    header.classList.add('fade-in');
-  }
-});
+.grid {
+    position: relative;
+    transform: translateY(125px);
+    column-count: 4;
+    column-gap: 0.5em;
+    padding-bottom: 1.25em;
+    margin: 1.25em; /* first number applies to top/bottom second number applies to left/right */
+    height: auto;
+    z-index: 0;
+}
 
-// when the user chooses an image for the lightbox
-document.querySelectorAll('.grid-item').forEach(item => {
+.grid .grid-item {
+    margin-bottom: 0.5em;
+}
 
-  item.addEventListener('click', () => {
+.grid-item img:hover {
+    cursor: pointer;
+}
 
-    const img = item.querySelector('img')
-    const src = img.getAttribute('src')
-    const lightbox_image_container = document.querySelector('.lightbox-img')
-    const lightbox_img = lightbox_image_container.querySelector('img')
-    const lightbox = document.querySelector('.lightbox')
-
-    const overlay = document.querySelector('.overlay');
-
-    lightbox_img.src = src
-
-    overlay.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
-
-    lightbox.style.display = 'flex';
-
-    let index = 0;
-    for (let [i, val] of order.entries()) { 
-      if (val == lightbox_img.src) {
-        index = i
-        break;
-      }
+@media (max-width:1024px) {
+    .grid {
+        column-count: 3;
     }
-    const right_arrow = document.querySelector('.right-arrow')
-    const left_arrow = document.querySelector('.left-arrow')
+    .bottom-section {
+        height: 200px;
+    }
+}
+@media (max-width:728px) {
+    .grid {
+        column-count: 2;
+    }
+    .bottom-section {
+        height: 150px;
+    }
+}
+@media (max-width:576px) {
+    .grid {
+        column-count: 1;
+    }
+    .bottom-section {
+        height: 100px;
+    }
+}
 
-    right_arrow.addEventListener('click', () => {
-      if (index + 1 >= order.length) {
-        index = 0
-        lightbox_img.src = order[0]
-      } else {
-        lightbox_img.src = order[index + 1]
-        index += 1
-      }
-    })
-    left_arrow.addEventListener('click', () => {
-      if (index - 1 < 0) {
-        index = order.length - 1
-        lightbox_img.src = order[order.length - 1]
-      } else {
-        lightbox_img.src = order[index - 1]
-        index -= 1
-      }
-    })
-    
-  })
-})
+/* image fading  */
+.fade-on-scroll {
+    opacity: 0;
+    transform: translateY(10px);
+    transition: opacity 0.3s ease, transform 0.3s ease;
+}
 
-// makes it so the browser behind the lightbox image darkens
-document.querySelector('.overlay').addEventListener('click', () => {
-    const lightbox = document.querySelector('.lightbox')
-    lightbox.style.display = 'none';
-    document.querySelector('.overlay').style.display = 'none';
-    document.body.style.overflow = 'visible'
-})
+.fade-on-scroll.visible {
+    opacity: 1;
+    transform: translateY(0);
+}
 
+.fade-out{
+    opacity: 0;
+    transition: opacity 0.5s ease, transform 0.5s ease;
+}
+.fade-in{
+    opacity: 1;
+    transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.index-header {
+    display: flex;
+    align-items: center;
+    background-color: #a9ba9d;
+    height: 125px;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+}
+
+h1 {
+    color: #687169;
+    transform: translateY(20px) translateX(20px);
+    font-family: "Libre Baskerville", serif;
+    font-weight: 700;
+    font-size: 30px;
+    margin: 0;
+}
+h2 {
+    transform: translateY(23px) translateX(50px);
+    color: #687169;
+    font-family: "Libre Baskerville", serif;
+    font-size: 20px;
+    font-weight: 700;
+}
+h3 {
+    transform: translateY(23px) translateX(75px);
+    color: #687169;
+    font-family: "Libre Baskerville", serif;
+    font-size: 20px;
+    font-weight: 700;
+}
+h4 {
+    transform: translateY(23px) translateX(100px);
+    color: #687169;
+    font-family: "Libre Baskerville", serif;
+    font-size: 20px;
+    font-weight: 700;
+}
+a {
+    text-decoration: none;
+}
+
+button {
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0;
+}
+
+.bottom-section {
+    background-color: #a9ba9d;
+    height: 300px;
+    width: 100vw;
+    transform: translateY(100px);
+}
+
+.overlay {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.lightbox {
+    display: none;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    z-index: 2;
+}
+.lightbox-img {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-50%);
+}
+.lightbox-img img {
+    width: 70vw;
+    height: auto;
+}
+.right-arrow { 
+    position: fixed;
+    transform: scale(3);
+    top: 50%;
+    right: 2.5%;
+    color: white;
+}
+.right-arrow:hover {
+    cursor: pointer;
+}
+.left-arrow {
+    position: fixed;
+    transform: scale(3);
+    top: 50%;
+    left: 2.5%;
+    color: white;
+}
+.left-arrow:hover {
+    cursor: pointer;
+}
